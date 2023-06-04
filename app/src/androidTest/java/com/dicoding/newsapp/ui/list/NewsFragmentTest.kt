74bbf50dee27ab5a9b490bda767682bbbe9dd44f
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-04 20:12:11 +0700

    implement idling resource

--- app/src/androidTest/java/com/dicoding/newsapp/ui/list/NewsFragmentTest.kt
+++ app/src/androidTest/java/com/dicoding/newsapp/ui/list/NewsFragmentTest.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
@@ -11,9 +13,11 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import com.dicoding.newsapp.JsonConverter
 import com.dicoding.newsapp.R
 import com.dicoding.newsapp.data.remote.retrofit.ApiConfig
+import com.dicoding.newsapp.util.EspressoIdlingResource
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
@@ -22,6 +26,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@MediumTest
 class NewsFragmentTest {
 
     private val mockWebServer = MockWebServer()
@@ -30,11 +35,13 @@ class NewsFragmentTest {
     fun setUp() {
         mockWebServer.start(8080)
         ApiConfig.BASE_URL = "http://127.0.0.1:8080/"
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
     }
 
     @After
     fun tearDown() {
         mockWebServer.shutdown()
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
     }
 
     @Test
