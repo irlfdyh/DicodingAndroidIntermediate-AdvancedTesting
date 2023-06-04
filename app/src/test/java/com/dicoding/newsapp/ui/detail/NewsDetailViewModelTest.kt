d5b7c7a2e702de84dc5f78b939aba5e48b201777
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-04 15:47:15 +0700

    create test dispatcher rule and implement it on news detail test

--- app/src/test/java/com/dicoding/newsapp/ui/detail/NewsDetailViewModelTest.kt
+++ app/src/test/java/com/dicoding/newsapp/ui/detail/NewsDetailViewModelTest.kt
@@ -2,6 +2,7 @@ package com.dicoding.newsapp.ui.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
+import com.dicoding.newsapp.MainDispatcherRule
 import com.dicoding.newsapp.data.NewsRepository
 import com.dicoding.newsapp.getOrAwaitValue
 import com.dicoding.newsapp.util.DataDummy
@@ -40,17 +41,8 @@ class NewsDetailViewModelTest {
         newsDetailViewModel.setNewsData(dummyDetailNews)
     }
 
-    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
-
-    @Before
-    fun setupDispatcher() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @After
-    fun tearDownDispatcher() {
-        Dispatchers.resetMain()
-    }
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
     @Test
     fun `when bookmarkStatus false should call saveNews`() = runTest {
